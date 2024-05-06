d6c479939309ff026c186720b05eaadd4d8e5dcb
Shreyashi~ <162280048+Shreyashi-chowdhury@users.noreply.github.com>
2024-05-06 13:04:16 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,35 +1,18 @@
-#include <iostream>
+ #include <iostream>
+ #include<string.h>
+
+ #include"calculate.h"
 using namespace std;
-class shape{
-    public:         //must right to access publicly
-        int l,b,h; //data member/member variable
-        void square(int len){             //member functions
-            l=len;
-            cout<<l*l;
-        }
-      
-        void rectangle(int len,int bre){
-            l=len;
-            b=bre;
-            cout<<l*b; 
-            }
-          
-        void triangle(int hei,int bre){
-            b=bre;
-            h=hei;
-            int a=0.5*b*h;
-            cout<<a;
-        }    
-        
-    };
+int main(){
+    int a;
+    int b;
+    char c;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cal(a,b,c);
 
-    int main(){
-        shape area; //class object; area>>object..calling funtion of class
-        area.square(4);
-        area.rectangle(2,3);
-        area.triangle(4,8);
-        return 0;
-    }
+    return 0;
+}
 
-
-
+                                                             
